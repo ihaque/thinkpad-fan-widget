6406f759cf2364fa50e7ff058aaad9f6d722f133
Imran Haque <ihaque@cs.stanford.edu>
2020-06-08 10:29:31 -0600

    small cleanups

--- widget.py
+++ widget.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+import os.path
+import re
+import signal
+from subprocess import run, PIPE
+
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
@@ -7,12 +13,6 @@ from gi.repository import GLib as glib
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
 
-import re
-import signal
-from subprocess import run, PIPE
-import os.path
-
-
 APPINDICATOR_ID = 'fanspeed_indicator'
 REFRESH_RATE_MS = 2500
 
@@ -20,11 +20,14 @@ def run_stdout(command):
     return run(command, stdout=PIPE).stdout.decode('utf-8')
 
 SENSORS_RX = re.compile(r'Package id 0: *\+(?P<temp_celsius>[0-9.]+).C')
+
+
 def get_temperature():
     log = run_stdout(['sensors', 'coretemp-isa-0000'])
     match = SENSORS_RX.search(log)
     assert match is not None, log
     return float(match.groupdict()['temp_celsius'])
+
 
 def set_fanspeed(speed):
     speed = str(speed)
@@ -33,9 +36,13 @@ def set_fanspeed(speed):
     run(['./set_fan_level', speed])
     return
 
+
 def menu_fanspeed_helper(menuitem, speed):
+    if speed == 'maximum':
+        speed = 'disengaged'
     set_fanspeed(speed)
     return True
+
 
 def get_fanspeed():
     data = {}
@@ -49,10 +56,14 @@ def get_fanspeed():
     # expect speed, status, level
     return data
 
+
 def update_label(ind_app):
     speed = get_fanspeed()
-    ind_app.set_label('%d°C %d rpm %s' % (get_temperature(), speed['speed'], speed['level'][0]), '')
+    if speed['level'] == 'disengaged':
+        speed['level'] = 'maximum'
+    ind_app.set_label('%d\u00b0C %d rpm %s' % (get_temperature(), speed['speed'], speed['level'][0]), '')
     return True
+
 
 def main():
     indicator = appindicator.Indicator.new(
@@ -67,18 +78,24 @@ def main():
     glib.timeout_add(REFRESH_RATE_MS, update_label, indicator)
     gtk.main()
 
+
 def build_menu():
     menu = gtk.Menu()
-    for level in list(range(0,8)) + ['automatic', 'disengaged']:
+    for level in ['automatic'] + list(range(0,8)) + ['maximum']:
         item = gtk.MenuItem('Level %s' % level)
         item.connect('activate', menu_fanspeed_helper, level)
         menu.append(item)
+    quit_item = gtk.MenuItem('Quit')
+    quit_item.connect('activate', quit)
+    menu.append(quit_item)
     menu.show_all()
     return menu
+
 
 def quit(_):
     notify.uninit()
     gtk.main_quit()
+
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
